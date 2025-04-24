50d6a24494b0f71714f90b46ca26644a7c927a0e
Inpayo <1np4y0@gmail.com>
2025-04-24 09:53:21 +0800

    Revert "Merge branch 'main' into afiq"
    This reverts commit 817430f2af8d78e2e68a0515b12cd9d4bea3d620, reversing
    changes made to 3b3cc4c67b291f53477934433786ff5dc710d2b9.

--- Scripts/player_testing.gd
+++ Scripts/player_testing.gd
@@ -8,23 +8,12 @@ var input_direction = Vector2.ZERO
 
 func get_input(_delta):
 	var input_direction = Input.get_vector("Move_left", "Move_right", "Move_up", "Move_down")
-<<<<<<< HEAD
-	if input_direction.length() > 0:
-		$Sprite2D/AnimationPlayer.play("Walking")
-		input_direction = input_direction.normalized()
-	if Input.is_action_pressed("sprint"):
-		speed = 1200
-	else:
-		speed = 600
-	velocity = input_direction * speed
-=======
 	input_direction = input_direction.normalized()
 	
 	velocity += input_direction * Acceleration * _delta
 	
 func Apply_friction(_delta):
 	velocity -= velocity * Friction * _delta
->>>>>>> 643f8f1a9467b8115979b909e39a003164fe818f
 	
 func _physics_process(_delta):
 	get_input(_delta)
