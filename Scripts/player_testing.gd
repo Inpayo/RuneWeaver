97c253b8c04262e41880d9135272981142016c5a
LIsSleepy <lwantsrest@gmail.com>
2025-04-24 10:30:51 +0800

    Ice? Trivago!
    Wouldn't it be rather "ice" to not feel like you're walking on ice all the time? Well here you go!

--- Scripts/player_testing.gd
+++ Scripts/player_testing.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
-@export var Speed = 800
-@export var Acceleration = 2400
-@export var Friction = Acceleration/Speed
+@export var Speed: float = 800
+@export var Acceleration: float = 2400
+@export var Friction: float = Acceleration/Speed
 var input_direction = Vector2.ZERO
 
 func get_input(_delta):
-	var input_direction = Input.get_vector("Move_left", "Move_right", "Move_up", "Move_down")
+	input_direction = Input.get_vector("Move_left", "Move_right", "Move_up", "Move_down")
 	input_direction = input_direction.normalized()
 	
 	velocity += input_direction * Acceleration * _delta
 	
 func Apply_friction(_delta):
-	velocity -= velocity * Friction * _delta
+	if input_direction == Vector2.ZERO:
+		velocity -= velocity * Friction ** 2 * _delta
+	else:
+		velocity -= velocity * Friction * _delta
 	
 func _physics_process(_delta):
 	get_input(_delta)
