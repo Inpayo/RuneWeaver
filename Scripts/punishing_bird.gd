b5cb700d34a66fbe1c14db2364036928d11528fe
Inpayo <1np4y0@gmail.com>
2025-04-29 19:23:52 +0800

    trying out new enemy movement, no success

--- Scripts/punishing_bird.gd
+++ Scripts/punishing_bird.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 600
 
 var dead = false
 var player_detected = false
+@onready var player_postion = $"../../CharacterBody2D/Hitbox/HurtyWurty"
 var player
 
 func _ready():
@@ -13,9 +14,13 @@ func _physics_process(_delta):
 	if !dead:
 		$Player_detection/CollisionShape2D.disabled = false
 		if player_detected:
-			position += (player.position - position) / speed
+			var direction = (player_postion-position).normalized()
+			velocity += direction * speed
 	else:
 		$Player_detection/CollisionShape2D.disabled = true
+		
+func Enemy_movement(CurrentX, CurrentY, ToX, ToY):
+	pass
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
