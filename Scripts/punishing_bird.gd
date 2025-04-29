a8ba3c9c48780932a9f9e19155264c3417264625
Inpayo <1np4y0@gmail.com>
2025-04-29 21:29:04 +0800

    fixed collision to enemies & improved enemy movement

--- Scripts/punishing_bird.gd
+++ Scripts/punishing_bird.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 300
 
 var dead = false
 var player_detected = false
+var direction = Vector2.ZERO
 var player
 
 func _ready():
@@ -13,9 +14,13 @@ func _physics_process(_delta):
 	if !dead:
 		$Player_detection/CollisionShape2D.disabled = false
 		if player_detected:
-			position += (player.position - position) / speed
+			direction = (player.position-position).normalized()
+			velocity = direction * speed
+		else:
+			velocity = Vector2.ZERO
 	else:
 		$Player_detection/CollisionShape2D.disabled = true
+	move_and_slide()
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
