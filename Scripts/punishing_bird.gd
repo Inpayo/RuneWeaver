718265aaa59b517df21e3bcaac0f8bf724284b4a
Inpayo <1np4y0@gmail.com>
2025-05-03 15:18:32 +0800

    Feat: enemies now tracks players last location

--- Scripts/punishing_bird.gd
+++ Scripts/punishing_bird.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var speed = 500
-
+var last_location
 var dead = false
 var player_detected = false
 var direction = Vector2.ZERO
@@ -12,14 +12,19 @@ func _ready():
 	
 func _physics_process(_delta):
 	if !dead:
-		$Player_detection/CollisionShape2D.disabled = false
+		$Player_detection/Detection.disabled = false
 		if player_detected:
-			direction = (player.position-position).normalized()
-			velocity = direction * speed
+			direction = position.direction_to(player.position)
+			velocity = direction * speed * _delta * 50
 		else:
-			velocity = Vector2.ZERO
+			if last_location != null:
+				if not (last_location - position) > Vector2(100,100) and not (last_location - position) < Vector2(-100,-100):
+					velocity = Vector2.ZERO
+				else:
+					direction = (last_location - position).normalized()
+					velocity = direction * speed * _delta * 50
 	else:
-		$Player_detection/CollisionShape2D.disabled = true
+		$Player_detection/Detection.disabled = true
 	move_and_slide()
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
@@ -30,4 +35,5 @@ func _on_player_detection_body_entered(body: Node2D) -> void:
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_detected = false
+		last_location = player.position
 		player = body
